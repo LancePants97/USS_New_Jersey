e7da58102526f8986954d4124f9fe5336ff31231
Lance Nelson <lancen04@gmail.com>
2024-04-06 14:06:49 -0400

    added methods to cell class and some corresponding tests in spec file

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,22 +1,20 @@
 class  Cell
     
-    attr_reader :coordinate
+    attr_reader :coordinate,
+                :ship
 
     def initialize(coordinate)
         @coordinate = coordinate
         @ship = nil
+        @empty = true
     end
 
     def empty?
         true
     end
 
-    def ship
-    
-    end
-
-    def place_ship(ship)
-
-    end
+    # def place_ship(ship)
+        
+    # end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,14 +2,36 @@ require 'spec_helper'
 
 RSpec.describe Cell do
 
-    it 'exists' do
-        expect(cell = Cell.new("B4")).to be_a(Cell)
+    describe '#initialize' do
+        it 'exists' do
+            expect(cell = Cell.new("B4")).to be_a(Cell)
+        end
+
+        it 'has a coordinate' do
+            cell = Cell.new("B4")
+            expect(cell.coordinate).to eq("B4")
+        end
     end
 
-    it 'has a coordinate' do
-        cell = Cell.new("B4")
-        expect(cell.coordinate).to eq("B4")
-    end
+
+        it 'has no ship by default' do
+            cell = Cell.new("B4")
+            expect(cell.ship).to eq(nil)
+        end
+
+        it 'is empty by default' do
+            cell = Cell.new("B4")
+            expect(cell.empty?).to be(true)
+        end
+
+        # it 'is empty by default' do
+        #     cell = Cell.new("B4")
+        #     cruiser = Ship.new("Cruiser", 3)
+
+        #     cell.place_ship(cruiser)
+
+        #     expect(cell.empty?).to be(false)
+        # end
     
 
 end
