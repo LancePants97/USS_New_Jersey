94f490e5067a85a800405fbb5dca84c7bf67e347
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-04-08 13:14:27 -0600

    change spacing on lib/cell file

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,54 +1,53 @@
 class  Cell
     
-    attr_reader :coordinate, 
-                :ship, 
-                :empty, 
-                :fired_upon
-                
+	attr_reader :coordinate, 
+							:ship, 
+							:empty, 
+							:fired_upon	
 
-    def initialize(coordinate)
-        @coordinate = coordinate
-        @ship = nil
-        @empty = true     
-        @fired_upon = false 
-    end
+	def initialize(coordinate)
+		@coordinate = coordinate
+		@ship = nil
+		@empty = true     
+		@fired_upon = false 
+	end
 
-    def empty?
-        @empty
-    end
+	def empty?
+		@empty
+	end
 
-    def place_ship(ship)
-        @empty = false
-        @ship = ship
-    end
+	def place_ship(ship)
+		@empty = false
+		@ship = ship
+	end
 
-    def fired_upon?
-        @fired_upon
-    end
+	def fired_upon?
+		@fired_upon
+	end
 
-    def fire_upon
-        @fired_upon = true
-        if @empty == false
-            register_hit
-        end
-    end
+	def fire_upon
+		@fired_upon = true
+		if @empty == false
+				register_hit
+		end
+	end
 
-    def register_hit
-        @ship.hit
-    end
+	def register_hit
+		@ship.hit
+	end
 
-    def render
-        if @fired_upon == false && @empty == true
-            "." # default empty cell
-        elsif @fired_upon == false && @empty == false
-            "S" # SHIP -cell containing a ship that hasn't been hit
-        elsif @fired_upon == true && @empty == true
-            "M" # MISS -cell containing no ship that has been hit
-        elsif @fired_upon == true && @empty == false && @ship.sunk? == false
-            "H" # HIT - cell containing a ship that has been hit
-        elsif @ship.sunk? == true
-            "X" # SUNK - cell containing a ship that has been sunk
-        end
-    
-    end
+	def render
+		if @fired_upon == false && @empty == true
+				"." # default empty cell
+		elsif @fired_upon == false && @empty == false
+				"S" # SHIP -cell containing a ship that hasn't been hit
+		elsif @fired_upon == true && @empty == true
+				"M" # MISS -cell containing no ship that has been hit
+		elsif @fired_upon == true && @empty == false && @ship.sunk? == false
+				"H" # HIT - cell containing a ship that has been hit
+		elsif @ship.sunk? == true
+				"X" # SUNK - cell containing a ship that has been sunk
+		end
+	
+	end
 end
