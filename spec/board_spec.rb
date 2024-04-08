b72aaea01c0bccf2810e2e2f6b82575272458a6e
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-04-08 13:41:00 -0600

    feature: first half of board tests

---
+++ spec/board_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+RSpec.describe Board do
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(board = Board.new).to be_a(Board)
+    end
+  end
+
+  describe '#cells' do
+    it 'has correct number of cells' do
+      expect(board.cells).to eq({}) #16 key/value pairs, keys point to cell objects
+    end
+  end
+
+  describe '#coordinate?' do
+    it 'has valid coordinates' do
+      expect(board.valid_coordinate?("A1")).to be true
+      expect(board.valid_coordinate?("D4")).to be true
+      expect(board.valid_coordinate?("A5")).to be false
+      expect(board.valid_coordinate?("E1")).to be false
+      expect(board.valid_coordinate?("A22")).to be false
+    end
+  end
+
+  describe '#valid_placement?' do
+    it 'can invalidate ship placement based on incorrect length of ship' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+    end
+
+    it 'can invalidate ship placements based on nonconsecutive coordinates' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+      expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+      expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+    end
+
+    it 'can invalidate ship placements based on diagonal' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+    end
+
+    it 'can validate ship placements based on validation conditions passing' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+    end
+
+  end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require './lib/ship'
 require './lib/cell'
+require './lib/board'
 
 RSpec.configure do |config|
   config.formatter = :documentation
